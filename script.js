49bf4bfc0133bad63623d63779b2fe320d453ff9
ManyToMany <sahar.zs2002@gmail.com>
2025-05-01 09:10:27 +0330

    selecting and manipulating an element

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.label-highscore').textContent = 'The highest score:';
+console.log(document.querySelector('.label-highscore').textContent);
+
+document.querySelector('.score').textContent = '12';
+console.log(document.querySelector('.score').textContent);
+
+document.querySelector('.guess').value = '18';
+console.log(document.querySelector('.guess').value);
+
+document.querySelector('.between').textContent =
+  'Enter a number between 1 to 20😉';
+console.log(document.querySelector('.between').textContent);
